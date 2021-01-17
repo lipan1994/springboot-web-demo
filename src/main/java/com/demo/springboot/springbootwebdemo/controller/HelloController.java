86b33612e745334be059ff45d75c74db16528f20
lipan <1757846472@qq.com>
2021-01-17 10:59:49 +0800

    增加自定义异常类

--- src/main/java/com/demo/springboot/springbootwebdemo/controller/HelloController.java
+++ src/main/java/com/demo/springboot/springbootwebdemo/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.demo.springboot.springbootwebdemo.controller;
 
+import com.demo.springboot.springbootwebdemo.exception.UserNotExistException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -20,5 +22,14 @@ public class HelloController {
     public  String success(Map<String,String> map){
         map.put("name","张三");
         return "success";
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(String user){
+        if("aaa".equals(user)){
+            throw  new UserNotExistException();
+        }
+        return "hello";
     }
 }
