8340303a2d834284cd1bd2358ccf80cdad2b1d54
lipan <1757846472@qq.com>
2021-01-17 10:39:01 +0800

    增加自定义异常类

---
+++ src/main/java/com/demo/springboot/springbootwebdemo/exception/UserNotExistException.java
@@ -0,0 +1,15 @@
+package com.demo.springboot.springbootwebdemo.exception;
+
+/**
+ * @author: lipan
+ * @date: 2021/1/17 10:34
+ */
+
+public class UserNotExistException extends RuntimeException {
+
+    public UserNotExistException() {
+        super();
+    }
+
+
+}
